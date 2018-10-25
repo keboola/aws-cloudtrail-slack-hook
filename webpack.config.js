5e79db7aac99fb4b3aaa0d87041e326ad4db1217
Jakub Matejka <jakub@keboola.com>
2018-10-25 11:20:01 +0200

    refactor: improve webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
       },
